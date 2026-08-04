1b12f00d99af152bec36c3496bcf3dcf636be020
Carolineamancio <carol.ama.simon@gmail.com>
2026-08-04 17:26:23 -0300

    fix: update script source path and set base URL for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/mascote-virtual-espacial/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
